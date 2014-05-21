5ec95dd33ea61fae401e7b800edd28c7b9548902
motemen <motemen@gmail.com>
2014-05-21 13:59:49 +0900

    doc

--- colorine.go
+++ colorine.go
@@ -6,66 +6,75 @@ import (
 	"github.com/daviddengcn/go-colortext"
 )
 
-type Color struct {
+// The logger. Create one with NewLogger().
+type Logger struct {
+	Prefixes     map[string]TextStyle
+	DefaultStyle TextStyle
+}
+
+type TextStyle struct {
+	Foreground TextColor
+	Background TextColor
+}
+
+type TextColor struct {
 	Color  ct.Color
 	Bright bool
 }
 
-type TextColor struct {
-	Foreground Color
-	Background Color
-}
-
+// Text color constants.
 var (
-	None = Color{ct.None, false}
+	None = TextColor{ct.None, false}
 
-	Black   = Color{ct.Black, false}
-	Red     = Color{ct.Red, false}
-	Green   = Color{ct.Green, false}
-	Yellow  = Color{ct.Yellow, false}
-	Blue    = Color{ct.Blue, false}
-	Magenta = Color{ct.Magenta, false}
-	Cyan    = Color{ct.Cyan, false}
-	White   = Color{ct.White, false}
+	Black   = TextColor{ct.Black, false}
+	Red     = TextColor{ct.Red, false}
+	Green   = TextColor{ct.Green, false}
+	Yellow  = TextColor{ct.Yellow, false}
+	Blue    = TextColor{ct.Blue, false}
+	Magenta = TextColor{ct.Magenta, false}
+	Cyan    = TextColor{ct.Cyan, false}
+	White   = TextColor{ct.White, false}
 
-	BrightBlack   = Color{ct.Black, true}
-	BrightRed     = Color{ct.Red, true}
-	BrightGreen   = Color{ct.Green, true}
-	BrightYellow  = Color{ct.Yellow, true}
-	BrightBlue    = Color{ct.Blue, true}
-	BrightMagenta = Color{ct.Magenta, true}
-	BrightCyan    = Color{ct.Cyan, true}
-	BrightWhite   = Color{ct.White, true}
+	BrightBlack   = TextColor{ct.Black, true}
+	BrightRed     = TextColor{ct.Red, true}
+	BrightGreen   = TextColor{ct.Green, true}
+	BrightYellow  = TextColor{ct.Yellow, true}
+	BrightBlue    = TextColor{ct.Blue, true}
+	BrightMagenta = TextColor{ct.Magenta, true}
+	BrightCyan    = TextColor{ct.Cyan, true}
+	BrightWhite   = TextColor{ct.White, true}
 )
 
+// Predefined text styles. Of course you can ignore these.
 var (
-	Verbose = TextColor{White, None}
-	Info    = TextColor{Green, None}
-	Notice  = TextColor{Blue, None}
-	Warn    = TextColor{Yellow, None}
-	Error   = TextColor{Red, None}
+	Verbose = TextStyle{White, None}
+	Info    = TextStyle{Green, None}
+	Notice  = TextStyle{Blue, None}
+	Warn    = TextStyle{Yellow, None}
+	Error   = TextStyle{Red, None}
 )
 
-type Logger struct {
-	Prefixes         map[string]TextColor
-	DefaultTextColor TextColor
+// Creates a new Logger.
+// prefixes is a prefix-string-to-text-style table. The log prefix is colored
+// using this table. If no entry is found in prefixes, defaultStyle is used.
+//
+// By default, no prefix is registered to any text style.
+func NewLogger(prefixes map[string]TextStyle, defaultStyle TextStyle) *Logger {
+	return &Logger{prefixes, defaultStyle}
 }
 
-func NewLogger(prefixes map[string]TextColor, defaultTextColor TextColor) *Logger {
-	return &Logger{prefixes, defaultTextColor}
-}
-
+// Log messages to console. prefix is colored using the logger.prefixes table.
 func (logger *Logger) Log(prefix, message string) {
-	textColor, ok := logger.Prefixes[prefix]
+	TextColor, ok := logger.Prefixes[prefix]
 	if !ok {
-		textColor = logger.DefaultTextColor
+		TextColor = logger.DefaultStyle
 	}
 
 	ct.ChangeColor(
-		textColor.Foreground.Color,
-		textColor.Foreground.Bright,
-		textColor.Background.Color,
-		textColor.Background.Bright,
+		TextColor.Foreground.Color,
+		TextColor.Foreground.Bright,
+		TextColor.Background.Color,
+		TextColor.Background.Bright,
 	)
 
 	fmt.Printf("%10s", prefix)
--- colorine_test.go
+++ colorine_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestLog(t *testing.T) {
 	logger := NewLogger(
-		map[string]TextColor{
+		map[string]TextStyle{
 			"verbose": Verbose,
 			"info":    Info,
 			"notice":  Notice,
@@ -19,4 +19,29 @@ func TestLog(t *testing.T) {
 	logger.Log("notice", "shows this color")
 	logger.Log("warn", "shows this color")
 	logger.Log("error", "shows this color")
+}
+
+func ExampleLogger() {
+	logger := NewLogger(
+		map[string]TextStyle{
+			"verbose": Verbose,
+			"info":    Info,
+			"notice":  Notice,
+			"warn":    Warn,
+			"error":   Error,
+		},
+		Info,
+	)
+
+	logger.Log("verbose", "shows this color")
+	logger.Log("info", "shows this color")
+	logger.Log("notice", "shows this color")
+	logger.Log("warn", "shows this color")
+	logger.Log("error", "shows this color")
+	// Output:
+	//[0;37m   verbose[0m shows this color
+	//[0;32m      info[0m shows this color
+	//[0;34m    notice[0m shows this color
+	//[0;33m      warn[0m shows this color
+	//[0;31m     error[0m shows this color
 }
