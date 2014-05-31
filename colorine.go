3aa5709e5b62adbf48279777dfc38dbc6c9899c4
motemen <motemen@gmail.com>
2014-05-31 15:53:48 +0900

    docs

--- colorine.go
+++ colorine.go
@@ -1,3 +1,15 @@
+// Package colorine provides a simple feature to print messages to console.
+//
+// A log line consists of a prefix and a message. The prefix is colored by its
+// content (for example below, "create" is green, "exists" is blue, and "error"
+// is red, and so on.) and the message is not colored.
+//	$ your_program
+//	    create path/to/file
+//	    exists path/to/another/file
+//	     error something went wrong
+//
+// By default, no prefix color is defined. You must specify prefix-to-color
+// mapping when you create a Logger. See the Logger's example.
 package colorine
 
 import (
@@ -6,46 +18,48 @@ import (
 	"github.com/daviddengcn/go-colortext"
 )
 
-// The logger. Create one with NewLogger().
+// A Logger handles the printing stuff. Create one with NewLogger().
 type Logger struct {
 	Prefixes     map[string]TextStyle
 	DefaultStyle TextStyle
 }
 
+// A TextStyle represents the style that strings are displayed with. Consists
+// of a foreground and a background, each of which is one of the text color constants.
 type TextStyle struct {
-	Foreground TextColor
-	Background TextColor
+	Foreground textColor
+	Background textColor
 }
 
-type TextColor struct {
+type textColor struct {
 	Color  ct.Color
 	Bright bool
 }
 
-// Text color constants.
+// Text color constants. Use these to build a TextStyle.
 var (
-	None = TextColor{ct.None, false}
+	None = textColor{ct.None, false}
 
-	Black   = TextColor{ct.Black, false}
-	Red     = TextColor{ct.Red, false}
-	Green   = TextColor{ct.Green, false}
-	Yellow  = TextColor{ct.Yellow, false}
-	Blue    = TextColor{ct.Blue, false}
-	Magenta = TextColor{ct.Magenta, false}
-	Cyan    = TextColor{ct.Cyan, false}
-	White   = TextColor{ct.White, false}
+	Black   = textColor{ct.Black, false}
+	Red     = textColor{ct.Red, false}
+	Green   = textColor{ct.Green, false}
+	Yellow  = textColor{ct.Yellow, false}
+	Blue    = textColor{ct.Blue, false}
+	Magenta = textColor{ct.Magenta, false}
+	Cyan    = textColor{ct.Cyan, false}
+	White   = textColor{ct.White, false}
 
-	BrightBlack   = TextColor{ct.Black, true}
-	BrightRed     = TextColor{ct.Red, true}
-	BrightGreen   = TextColor{ct.Green, true}
-	BrightYellow  = TextColor{ct.Yellow, true}
-	BrightBlue    = TextColor{ct.Blue, true}
-	BrightMagenta = TextColor{ct.Magenta, true}
-	BrightCyan    = TextColor{ct.Cyan, true}
-	BrightWhite   = TextColor{ct.White, true}
+	BrightBlack   = textColor{ct.Black, true}
+	BrightRed     = textColor{ct.Red, true}
+	BrightGreen   = textColor{ct.Green, true}
+	BrightYellow  = textColor{ct.Yellow, true}
+	BrightBlue    = textColor{ct.Blue, true}
+	BrightMagenta = textColor{ct.Magenta, true}
+	BrightCyan    = textColor{ct.Cyan, true}
+	BrightWhite   = textColor{ct.White, true}
 )
 
-// Predefined text styles. Of course you can ignore these.
+// Predefined TextStyles. Of course you can ignore these.
 var (
 	Verbose = TextStyle{White, None}
 	Info    = TextStyle{Green, None}
@@ -54,27 +68,28 @@ var (
 	Error   = TextStyle{Red, None}
 )
 
-// Creates a new Logger.
-// prefixes is a prefix-string-to-text-style table. The log prefix is colored
-// using this table. If no entry is found in prefixes, defaultStyle is used.
+// NewLogger creates a new Logger.  prefixes is a prefix-string-to-text-style
+// table. The log prefix is colored using this table. If no entry is found in
+// prefixes, defaultStyle is used.
 //
 // By default, no prefix is registered to any text style.
 func NewLogger(prefixes map[string]TextStyle, defaultStyle TextStyle) *Logger {
 	return &Logger{prefixes, defaultStyle}
 }
 
-// Log messages to console. prefix is colored using the logger.prefixes table.
+// Log prints messages to console. prefix is colored using the logger.prefixes
+// table.
 func (logger *Logger) Log(prefix, message string) {
-	TextColor, ok := logger.Prefixes[prefix]
+	textColor, ok := logger.Prefixes[prefix]
 	if !ok {
-		TextColor = logger.DefaultStyle
+		textColor = logger.DefaultStyle
 	}
 
 	ct.ChangeColor(
-		TextColor.Foreground.Color,
-		TextColor.Foreground.Bright,
-		TextColor.Background.Color,
-		TextColor.Background.Bright,
+		textColor.Foreground.Color,
+		textColor.Foreground.Bright,
+		textColor.Background.Color,
+		textColor.Background.Bright,
 	)
 
 	fmt.Printf("%10s", prefix)
--- colorine_test.go
+++ colorine_test.go
@@ -24,24 +24,18 @@ func TestLog(t *testing.T) {
 func ExampleLogger() {
 	logger := NewLogger(
 		map[string]TextStyle{
-			"verbose": Verbose,
-			"info":    Info,
-			"notice":  Notice,
-			"warn":    Warn,
-			"error":   Error,
+			// Using colorine's predefined TextStyles
+			"create": Info,
+			"exists": Notice,
+			// Or specify your own
+			"debug": TextStyle{White, None},
+			"error": TextStyle{BrightRed, White},
 		},
-		Info,
+		// The default prefix color
+		TextStyle{Green, None},
 	)
 
-	logger.Log("verbose", "shows this color")
-	logger.Log("info", "shows this color")
-	logger.Log("notice", "shows this color")
-	logger.Log("warn", "shows this color")
-	logger.Log("error", "shows this color")
-	// Output:
-	//[0;37m   verbose[0m shows this color
-	//[0;32m      info[0m shows this color
-	//[0;34m    notice[0m shows this color
-	//[0;33m      warn[0m shows this color
-	//[0;31m     error[0m shows this color
+	logger.Log("create", "path/to/file")
+	logger.Log("exists", "path/to/another/file")
+	logger.Log("error", "something went wrong")
 }
